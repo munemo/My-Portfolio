c2c8d50d26e2423762f8aa7f135bd87a8740433d
munemo <john.chimbani@outlook.com>
2020-09-04 13:42:04 +0200

    Initial scaffolding functional

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { Component } from "react";
 import ProjectCard from "./ProjectCard"
+import { UndrawDashboard } from "react-undraw-illustrations";
 
 class Projects extends Component {
   state = {
@@ -29,9 +30,11 @@ class Projects extends Component {
     if (projects.length > 0) {
       projectsList = projects.map((project) => {
         return (
+          
           <div id={"project-" + project.id} key={project.id}>
             <ProjectCard project={project} />
           </div>
+
         );
       });
     }
@@ -40,12 +43,22 @@ class Projects extends Component {
 
     return (
       <div className="ui main container">
-        <h1 id="projects-header" className="ui header">
-          My Projects
-        </h1>
-        {projectsList}
+        <div className="ui stackable two column grid">
+          <div className="column">
+            <UndrawDashboard />
+          </div>
+          <div className="column">
+            <h1 className="ui header">My Projects</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+              quod ab doloremque eaque. Consequatur temporibus, quos enim, eaque
+              nemo ad iusto sequi modi totam qui veniam? Ab asperiores inventore
+              distinctio.
+           </p>
+          </div>
+        </div>
+        <div className="ui stackable four column grid">{projectsList}</div>
       </div>
-
     );
   }
 }
